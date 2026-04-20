8f2796475ec43469810c9656b4eb0a14834dd994
Delaynie <delaynieym@outlook.com>
2026-04-20 10:56:12 -0400

    testbench demonstrates functional sleep/wake
    press button to go to sleep, press again to wake up.
    device successfully wakes up again by printing "Woke up from button press!" in while true loop.

--- gnss-testbench/src/main.cpp
+++ gnss-testbench/src/main.cpp
@@ -8,9 +8,13 @@
 // the RX pin on the ESP32 that should connect to the GPS
 #define GPS_RX 5
 
-#define BUTTON_1 14
-#define BUTTON_2 15
-#define BUTTON_3 16
+// battery monitor pin
+#define BATT_MON 7
+
+#define BUTTON_1 14 // for sleep/wake
+#define BUTTON_2 15 // synchronize RTC with GNSS
+#define BUTTON_3 16 // print out stuff
+#define DEBOUNCE 100
 
 // Convert day of week number to abbreviated day name
 const char *getDayOfWeekName(uint8_t dayOfWeek)
@@ -30,46 +34,82 @@ const char *getMonthName(uint8_t month)
 gnss_time::DateTime datetime;
 unsigned long last_rtc_update;
 
+void wait_for_button_release(int pin_num)
+{
+  while (digitalRead(pin_num))
+  {
+    delay(10);
+  }
+  delay(DEBOUNCE);
+}
+
+void go_to_sleep()
+{
+  Serial.println("Button pressed, preparing to sleep...");
+
+  // CRITICAL: wait for release so it doesn't instantly wake back up
+  // because logic level HIGH is immediately detected
+  wait_for_button_release(BUTTON_1);
+
+  Serial.println("Going to sleep now.");
+  delay(100);
+
+  esp_deep_sleep_start();
+}
+
 void setup()
 {
-  pinMode(BUTTON_1, INPUT_PULLDOWN); // begin the program
-  pinMode(BUTTON_2, INPUT_PULLDOWN); // synchronize RTC with GNSS
-  pinMode(BUTTON_3, INPUT_PULLDOWN); // print all the stuff
-
-  while (!digitalRead(BUTTON_1))
-  {
-    // Serial.println("Press Button 1 to begin");
-  }
+  pinMode(BUTTON_1, INPUT_PULLDOWN);
+  pinMode(BUTTON_2, INPUT_PULLDOWN);
+  pinMode(BUTTON_3, INPUT_PULLDOWN);
+  analogReadMilliVolts(BATT_MON);
 
   Serial.begin(115200);
   Serial.println("Beginning");
+
+  esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+  if (cause == ESP_SLEEP_WAKEUP_EXT0)
+  {
+    while (1)
+    {
+      Serial.println("Woke up from button press!");
+      delay(500);
+    }
+  }
+  else
+  {
+    Serial.println("Fresh boot");
+  }
+
+  // Configure wake when button goes HIGH
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)BUTTON_1, 1);
+
   gnss_time::init(GPS_TX, GPS_RX, 300);
-  gnss_time::get_gnss_datetime(0, &datetime);
-  last_rtc_update = millis();
+  // gnss_time::get_gnss_datetime(0, &datetime);
+  // last_rtc_update = millis();
 }
 
 void loop()
 {
+  if (digitalRead(BUTTON_1))
+  {
+    go_to_sleep();
+  }
+
   if (digitalRead(BUTTON_2))
   { // syncrhonize RTC with GNSS
     gnss_time::get_gnss_datetime(0, &datetime);
     last_rtc_update = millis();
-    Serial.println("Updated RTC");
-
-    delay(100);
+    Serial.println("--------Updated RTC.--------");
   }
 
   if (digitalRead(BUTTON_3))
   {
+    Serial.println("----------------------------");
+
     // query SIV
     int SIV = gnss_time::get_SIV();
     Serial.printf("Satellites in view: %d\n", SIV);
-
-    // query RTC
-    gnss_time::only_get_rtc_datetime(&datetime);
-    Serial.printf("RTC: %s, %s %d, %d  %02d:%02d:%02d (UTC)\n",
-                  getDayOfWeekName(datetime.day_of_week), getMonthName(datetime.month), datetime.day, datetime.year,
-                  datetime.hour, datetime.minute, datetime.second);
 
     // query GNSS
     gnss_time::only_get_gnss_datetime(&datetime);
@@ -77,12 +117,23 @@ void loop()
                   getDayOfWeekName(datetime.day_of_week), getMonthName(datetime.month), datetime.day, datetime.year,
                   datetime.hour, datetime.minute, datetime.second);
 
-    // calculate drift?
+    // query RTC
+    gnss_time::only_get_rtc_datetime(&datetime);
+    Serial.printf("RTC: %s, %s %d, %d  %02d:%02d:%02d (UTC)\n",
+                  getDayOfWeekName(datetime.day_of_week), getMonthName(datetime.month), datetime.day, datetime.year,
+                  datetime.hour, datetime.minute, datetime.second);
 
-    Serial.println();
+    // time since last RTC update
+    int minutes = last_rtc_update / 60000;
+    float seconds = (float)(minutes % 60000) / 1000.0;
+    Serial.printf("RTC was updated %d min, %2.3f sec ago. (%lu ms)\n", minutes, seconds, last_rtc_update);
 
-    delay(100);
+    // battery level
+    uint16_t adc_batt_mv = analogReadMilliVolts(BATT_MON);
+    float batt_mv = (float)(adc_batt_mv * 370) / (float)270; // converting from a 100 k over 270 k voltage divider
+    Serial.printf("Battery monitor ADC pin = %1.3f V (battery = %1.3f V)\n", (float)adc_batt_mv / 1000.0, batt_mv / 1000.0);
+    Serial.println("----------------------------");
   }
 
-  delay(10);
+  delay(DEBOUNCE);
 }
